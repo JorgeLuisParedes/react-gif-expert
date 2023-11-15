30b83b7e219b14e7a5eefb461a5ddaebb2869863
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2023-11-15 22:57:16 +0100

    Pruebas en GifItem

---
+++ test/components/GifItem.test.jsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import { GifItem } from "../../src/components/GifItem";
+
+describe('Pruebas en <GifItem />', () => {
+
+    const title = 'Título de mi imagen';
+    const url = 'https://url-de-mi-imagen.com/mi-imagen.jpg';
+
+    test('Debe hacer match con el snapshot', () => {
+
+        const { container } = render( <GifItem title={ title } url={ url } /> );
+        expect( container ).toMatchSnapshot();
+
+    });
+
+    test('Debe de mostrar la imagen con el URL y el ALT indicado', () => {
+
+        render( <GifItem title={ title } url={ url } /> );
+        // screen.debug();
+        // expect( screen.getByRole( 'img' ).src ).toBe( url );
+        const { src, alt } = screen.getByRole('img');
+        expect( src ).toBe( url );
+        expect( alt ).toBe( title );
+
+    });
+
+    test('Debe de mostrar el título en el componente', () => {
+
+        render( <GifItem title={ title } url={ url } /> );
+        expect( screen.getByText( title ) ).toBeTruthy();
+
+    });
+
+});
